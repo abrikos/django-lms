3b4e376b86aab879c68463bf91b87ffef7fe8370
Artem Filippow <a.filippov@qtech.ru>
2025-08-05 13:22:49 +0900

    add models

--- config/settings.py
+++ config/settings.py
@@ -96,18 +96,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    # },
 ]
 
 
@@ -133,3 +133,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Name', null=True)
+    desc = models.CharField(verbose_name='Desc', null=True)
+    image = models.ImageField(upload_to='course/', verbose_name='Course picture', null=True)
+
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Name', null=True)
+    desc = models.CharField(verbose_name='Desc', null=True)
+    url = models.CharField(verbose_name='Video url', null=True)
+    image = models.ImageField(upload_to='course/', verbose_name='Lesson picture', null=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='course', null=True)
