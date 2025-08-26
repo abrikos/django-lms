00f9a343a0792c91e14112ea5805b3e96de75da1
Artem Filippow <a.filippov@qtech.ru>
2025-08-26 20:57:17 +0900

    refresh endpoint

--- config/urls.py
+++ config/urls.py
@@ -18,13 +18,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('lms/', include('lms.urls')),
     path('user/', include('users.urls')),
-    path(r'token/', TokenObtainPairView.as_view(), name='token_obtain_pair')
+    path(r'token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
